88af85501ceb641c884dae6d905628f917a33e27
billmannd <billmannd@gmail.com>
2016-08-23 10:42:09 -0400

    Erased comments

---
+++ College_Budget_App/src/test2.java
@@ -0,0 +1,17 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 5;
+		int multiply =4;
+		
+		int answer = 0;
+		
+		answer = number * multiply;
+		
+		System.out.println(answer);
+	}
+
+}
